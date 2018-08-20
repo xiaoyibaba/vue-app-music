6b2151490049a0f96057797ed3e36e356cd866b4
Mot-w <319776193@qq.com>
2018-08-21 02:07:39 +0800

    slider组件初步

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="slider" ref="slider">
+        <div class="slider-group" ref="sliderGroup">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { addClass } from 'common/js/dom'
+    import BScroll from 'better-scroll'
+
+    export default {
+        name: 'slider',
+        props:{
+            loop: {
+                type: Boolean,
+                default: true
+            },
+            autoPlay: {
+                type: Boolean,
+                default: true
+            },
+            interval: {
+                type: Number,
+                default: 4000
+            }
+        },
+        
+        mounted() {
+            setTimeout(() => {
+                this._setSliderWidth()
+                this._initSlider()
+            }, 20)
+        },
+
+        methods: {
+            _setSliderWidth() {
+                this.children = this.$refs.sliderGroup.children
+
+                let width = 0
+                let sliderWidth = this.$refs.slider.clientWidth
+                for (let i = 0; i < this.children.length; i++) {
+                    let child = this.children[i]
+                    addClass(child, 'slider-item')
+
+                    child.style.width = sliderWidth + 'px'
+                    width += sliderWidth
+                }
+                if (this.loop) {
+                    width += 2 * sliderWidth
+                }
+                this.$refs.sliderGroup.style.width = width + 'px'
+            },
+            _initSlider() {
+                this.slider = new BScroll(this.$refs.slider, {
+                    scrollX: true,
+                    scrollY: false,
+                    momentum: false,
+                    snap: {
+                        loop: this.loop,
+                        threshold: 0.3,
+                        speed: 400
+                    },
+                    click: true
+                })
+            }
+        }
+    }
+</script>
+
+
+<style lang="stylus" scoped>
+    @import "~common/stylus/variable"
+
+    .slider
+        min-height 1px
+        .slider-group 
+            position relative
+            ovrflow hidden 
+            white-space nowrap
+            .slider-item
+                float left
+                box-sizing: border-box
+                overflow hidden
+                text-align center
+                a 
+                    display block
+                    width 100%
+                    overflow hidden
+                    text-decoration none
+                img 
+                    display block
+                    width 100%
+</style>
+
+
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,14 +1,38 @@
 <template>
-    <div>
-        推荐页面
+    <div class="recommond">
+        <div class="recommond-content">
+            <div class="slider-wrap" v-if="recommonds.length">
+                <slider>
+                    <div v-for="item in recommonds">
+                        <a :href="item.linkUrl">
+                            <img :src="item.picUrl" alt="">
+                        </a>
+                    </div>
+                </slider>
+            </div>
+            <div class="recommond-list">
+                <h1 class="list-title">热门歌单</h1>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import {getRecommend} from 'api/recommend'
     import {ERR_OK} from 'api/config'
+    import Slider from 'base/slider/slider'
 
     export default {
+        components: {
+            Slider
+        },
+
+        data() {
+            return {
+                recommonds: []
+            }
+        },
+
         created() {
             this._getRecommend()
         },
@@ -16,13 +40,36 @@
         methods: {
             _getRecommend() {
                 getRecommend().then((res) => {
-                    console.log(res.code)
                     if(res.code === ERR_OK) {
-                        console.log(res.data.slider)
+                        this.recommonds = res.data.slider
                     }
                 })
             }
         }
     }
 </script>
+
+<style lang="stylus" scoped>
+    @import '~common/stylus/variable'
+    .recommond
+        position fixed
+        width 100%
+        top 88px
+        bottom 0
+        .recommond-content
+            height 100%
+            overflow hidden
+            .slider-wrap
+                position relative
+                width 100%
+                overflow hidden
+            .recommond-list
+                .list-title
+                    height 65px
+                    line-height 65px
+                    text-align center
+                    font-size $font-size-medium
+                    color $color-theme
+</style>
+
 
