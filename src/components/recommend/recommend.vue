b8b51f5555628d8f5ae15dab2466861c78ad20f9
Mot-w <319776193@qq.com>
2018-08-28 02:12:58 +0800

    列表页完成，初步抽象出scroll组件

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,40 +1,57 @@
 <template>
     <div class="recommond">
-        <div class="recommond-content">
-            <div class="slider-wrap" v-if="recommonds.length">
-                <slider>
-                    <div v-for="item in recommonds">
-                        <a :href="item.linkUrl">
-                            <img :src="item.picUrl" alt="">
-                        </a>
-                    </div>
-                </slider>
+        <scroll class="recommond-content" :data="discList">
+            <div>
+                <div class="slider-wrap" v-if="recommonds.length">
+                    <slider>
+                        <div v-for="item in recommonds">
+                            <a :href="item.linkUrl">
+                                <img :src="item.picUrl" alt="">
+                            </a>
+                        </div>
+                    </slider>
+                </div>
+                <div class="recommond-list">
+                    <h1 class="list-title">热门歌单</h1>
+                    <ul>
+                        <li v-for="item in discList" class="item">
+                            <div class="icon">
+                                <img :src="item.imgurl" width="60" height="60" alt="">
+                            </div>
+                            <div class="text">
+                                <h2 class="name" v-html="item.creator.name"></h2>
+                                <p class="desc" v-html="item.dissname"></p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div class="recommond-list">
-                <h1 class="list-title">热门歌单</h1>
-            </div>
-        </div>
+        </scroll>
     </div>
 </template>
 
 <script>
-    import {getRecommend} from 'api/recommend'
+    import scroll from 'base/scroll/scroll'
+    import {getRecommend, getDiscList} from 'api/recommend'
     import {ERR_OK} from 'api/config'
     import Slider from 'base/slider/slider'
 
     export default {
         components: {
-            Slider
+            Slider,
+            scroll
         },
 
         data() {
             return {
-                recommonds: []
+                recommonds: [],
+                discList: []
             }
         },
 
         created() {
-            this._getRecommend()
+            this._getRecommend(),
+            this._getDiscList()
         },
         
         methods: {
@@ -43,6 +60,12 @@
                     if(res.code === ERR_OK) {
                         this.recommonds = res.data.slider
                     }
+                })
+            },
+            _getDiscList() {
+                getDiscList().then(res => {
+                    console.log(res.data.list)
+                    this.discList = res.data.list
                 })
             }
         }
@@ -70,6 +93,29 @@
                     text-align center
                     font-size $font-size-medium
                     color $color-theme
+                .item 
+                    display flex
+                    box-sizing border-box
+                    align-items center
+                    padding 0 20px 20px 20px
+                    .icon
+                        flex 0 0 60px
+                        width 60px
+                        padding-right 20px
+                    .text
+                        display flex
+                        flex-direction column
+                        justify-content center
+                        flex 1
+                        line-height 20px
+                        overflow hidden
+                        font-size $font-size-medium
+                        .name
+                            margin-bottom 10px
+                            color $color-text
+                        .desc
+                            color $color-text-d
+
 </style>
 
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router/index'
 import fastclick from 'fastclick'
+import axios from 'axios'
+
+Vue.prototype.$ajax = axios
+axios.defaults.headers.common['Content-Type'] = 'application/json'
+axios.defaults.baseURL = 'http://localhost:3000'
 
 import 'common/stylus/index.styl'
 
